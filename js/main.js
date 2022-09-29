027cb41985cc97ee3ac0dbe2d52dbdc2ace349ea
Seyyednavid Hejazijouybari <100324062+seyyednavid@users.noreply.github.com>
2022-09-29 14:45:59 +0100

    Add new link for pic

--- js/main.js
+++ js/main.js
@@ -4,27 +4,27 @@ var i;
 
 i = 1;
 let element_img = document.getElementById('img');
-element_img.setAttribute("src", 'https://s3.uupload.ir/files/navid68/python1.jpeg');
+element_img.setAttribute("src",'https://files.fm/thumb_show.php?i=38cpja4kv');
 
 
 document.getElementById('next').addEventListener('click', (event) => {
   i = (typeof i === 'number' ? i : 0) + 1;
   if (i == 2) {
     let element_img2 = document.getElementById('img');
-    element_img2.setAttribute("src", 'https://s3.uupload.ir/files/navid68/sql2.jpeg');
+    element_img2.setAttribute("src", 'https://files.fm/thumb_show.php?i=u2cxk5d6z');
   } else if (i == 3) {
     let element_img3 = document.getElementById('img');
-    element_img3.setAttribute("src", 'https://s3.uupload.ir/files/navid68/R3.jpeg');
+    element_img3.setAttribute("src", 'https://files.fm/thumb_show.php?i=zdqka7f8z');
   } else if (i == 4) {
     let element_img4 = document.getElementById('img');
-    element_img4.setAttribute("src", 'https://s3.uupload.ir/files/navid68/docker4.jpeg');
+    element_img4.setAttribute("src", 'https://files.fm/thumb_show.php?i=umxkhmmfp');
   } else if (i == 5) {
     let element_img5 = document.getElementById('img');
-    element_img5.setAttribute("src", 'https://s3.uupload.ir/files/navid68/Ml5.jpeg');
+    element_img5.setAttribute("src", 'https://files.fm/thumb_show.php?i=9n7pb2ma2');
   } else if (i == 6) {
     i = 1;
     let element_img6 = document.getElementById('img');
-    element_img6.setAttribute("src", 'https://s3.uupload.ir/files/navid68/python1.jpeg');
+    element_img6.setAttribute("src", 'https://files.fm/thumb_show.php?i=38cpja4kv');
   }
 
 });
@@ -33,19 +33,19 @@ document.getElementById('previous').addEventListener('click', (event) => {
   i = (typeof i === 'number' ? i : 0) + -1;
   if (i == 1) {
     let element_img7 = document.getElementById('img');
-    element_img7.setAttribute("src", 'https://s3.uupload.ir/files/navid68/python1.jpeg');
+    element_img7.setAttribute("src", 'https://files.fm/thumb_show.php?i=u2cxk5d6z');
   } else if (i == 2) {
     let element_img8 = document.getElementById('img');
     element_img8.setAttribute("src", 'https://s3.uupload.ir/files/navid68/sql2.jpeg');
   } else if (i == 3) {
     let element_img9 = document.getElementById('img');
-    element_img9.setAttribute("src", 'https://s3.uupload.ir/files/navid68/R3.jpeg');
+    element_img9.setAttribute("src", 'https://files.fm/thumb_show.php?i=zdqka7f8z');
   } else if (i == 4) {
     let element_img10 = document.getElementById('img');
-    element_img10.setAttribute("src", 'https://s3.uupload.ir/files/navid68/docker4.jpeg');
+    element_img10.setAttribute("src", 'https://files.fm/thumb_show.php?i=umxkhmmfp');
   } else if (i == 5) {
     let element_img11 = document.getElementById('img');
-    element_img11.setAttribute("src", 'https://s3.uupload.ir/files/navid68/Ml5.jpeg');
+    element_img11.setAttribute("src", 'https://files.fm/thumb_show.php?i=9n7pb2ma2');
   } else if (i == 0) {
     i = 5;
     let element_img12 = document.getElementById('img');
